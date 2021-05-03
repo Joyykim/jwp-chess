9e2b9a64ed7527160acd558e2d6280b4862d5fce
김주원 <kjw11077@naver.com>
2021-05-03 20:58:45 +0900

    feat: 로그

--- src/main/java/chess/SpringChessApplication.java
+++ src/main/java/chess/SpringChessApplication.java
@@ -1,5 +1,7 @@
 package chess;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.stereotype.Controller;
@@ -9,12 +11,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class SpringChessApplication {
 
+	private static final Logger log = LoggerFactory.getLogger(Controller.class);
+	private static final Logger fileLogger = LoggerFactory.getLogger("file");
+
 	public static void main(String[] args) {
 		SpringApplication.run(SpringChessApplication.class, args);
 	}
 
 	@GetMapping("/")
 	public String index() {
+		log.error("An ERROR Message");
+		fileLogger.info("파일 로깅 입니다.");
 		return "index";
 	}
 }
